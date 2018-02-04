adb8ae9fcb9db89363741ca7f10be9e014c366f8
Grigorii Kasymov <kasymovga@gmail.com>
2018-02-04 16:04:47 +0500

    fixed wrong cvar name

--- qcsrc/menu/nexuiz/dialog_settings_effects.c
+++ qcsrc/menu/nexuiz/dialog_settings_effects.c
@@ -92,7 +92,7 @@ void fillNexuizEffectsSettingsTab(entity me)
 		me.TD(me, 1, 2, e = makeNexuizSlider(0, 100, 10, "cl_gibs_maxcount"));
 	me.TR(me);
 		me.TD(me, 1, 1, e = makeNexuizTextLabel(0, "Casing count:"));
-		me.TD(me, 1, 2, e = makeNexuizSlider(0, 100, 10, "cl_casing_maxcount"));
+		me.TD(me, 1, 2, e = makeNexuizSlider(0, 100, 10, "cl_casings_maxcount"));
 	me.TR(me);
 		me.TD(me, 1, 3, e = makeNexuizCheckBox(0, "cl_decals", "Decals"));
 	me.TR(me);
