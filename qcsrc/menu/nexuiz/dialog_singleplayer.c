8d33b38922c39f38ad04a86a726a15f7eb370a62
Grigorii Kasymov <kasymovga@gmail.com>
2018-10-14 18:14:41 +0500

    single player demo

--- qcsrc/menu/nexuiz/dialog_singleplayer.c
+++ qcsrc/menu/nexuiz/dialog_singleplayer.c
@@ -9,6 +9,7 @@ CLASS(NexuizSingleplayerDialog) EXTENDS(NexuizDialog)
 	ATTRIB(NexuizSingleplayerDialog, campaignBox, entity, NULL)
 	ATTRIB(NexuizSingleplayerDialog, btnSurv, entity, NULL)
 	ATTRIB(NexuizSingleplayerDialog, btnAT, entity, NULL)
+	ATTRIB(NexuizSingleplayerDialog, btnSP, entity, NULL)
 ENDCLASS(NexuizSingleplayerDialog)
 #endif
 
@@ -20,8 +21,11 @@ void SwitchTo(entity btn, entity me) {
 	btn.forcePressed = 1;
 	if (btn == me.btnSurv)
 		cvar_set("g_campaign_name", "rexsurvival");
-	else
+	else if (btn == me.btnAT)
 		cvar_set("g_campaign_name", "rexuiz");
+	else
+		cvar_set("g_campaign_name", "spdemo");
+
 	me.campaignBox.loadCvars(me.campaignBox);
 }
 
@@ -30,19 +34,21 @@ void fillNexuizSingleplayerDialog(entity me)
 	entity e;
 
 	me.TR(me);
-		me.TD(me, 1, 2.5, me.btnSurv = makeNexuizButton("Survival Campaign", '0 0 0'));
+		me.TD(me, 1, 1.5, me.btnSurv = makeNexuizButton("Survival Campaign", '0 0 0'));
 		me.btnSurv.onClick = SwitchTo;
 		me.btnSurv.onClickEntity = me;
-		me.TD(me, 1, 2.5, me.btnAT = makeNexuizButton("Arena Training", '0 0 0'));
+		me.TD(me, 1, 1.5, me.btnAT = makeNexuizButton("Arena Training", '0 0 0'));
 		me.btnAT.onClick = SwitchTo;
 		me.btnAT.onClickEntity = me;
+		me.TD(me, 1, 1.5, me.btnSP = makeNexuizButton("Single Player (Demo)", '0 0 0'));
+		me.btnSP.onClick = SwitchTo;
+		me.btnSP.onClickEntity = me;
 	me.TR(me);
 	me.TR(me);
 		me.TD(me, me.rows - 4, me.columns, me.campaignBox = makeNexuizCampaignList());
 	SwitchTo(me.btnSurv, me);
 	me.gotoRC(me, me.rows - 1, 0);
-		//me.TD(me, 1, 2, e = makeNexuizModButton("Singleplayer"));
-		me.TD(me, 1, me.columns , e = makeNexuizButton("Start Singleplayer!", '0 0 0'));
+		me.TD(me, 1, me.columns , e = makeNexuizButton("Start Game!", '0 0 0'));
 			e.onClick = CampaignList_LoadMap;
 			e.onClickEntity = me.campaignBox;
 }
