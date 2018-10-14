312b1aa30e0c30c58a4f0799539eb26037ee7c39
Grigorii Kasymov <kasymovga@gmail.com>
2018-10-14 11:49:00 +0500

    element placement in create dialog

--- qcsrc/menu/nexuiz/dialog_multiplayer_create.c
+++ qcsrc/menu/nexuiz/dialog_multiplayer_create.c
@@ -32,7 +32,7 @@ void fillNexuizServerCreateTab(entity me)
 	float n;
 
 	float i;
-	n = 8;
+	n = 6;
 	selected = NULL;
 	for (i = GAME_DEATHMATCH; i < GAME_SINGLE; i++) {
 		if (mod(i - 1, n) == 0)
@@ -55,7 +55,7 @@ void fillNexuizServerCreateTab(entity me)
 		me.TD(me, 1, 3, e = makeNexuizTextLabel(0, "Map list:"));
 			makeCallback(e, me.mapListBox, me.mapListBox.refilterCallback);
 	me.TR(me);
-		me.TD(me, me.rows - 7, 3, me.mapListBox);
+		me.TD(me, me.rows - 8, 3, me.mapListBox);
 	me.gotoRC(me, me.rows - 3, 0);
 		me.TDempty(me, 0.5);
 		me.TD(me, 1, 1, e = makeNexuizButton("All", '0 0 0'));
