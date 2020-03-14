b7302ebfba32ff46c5856e1054587fb0b321cfdc
Grigorii Kasymov <kasymovga@gmail.com>
2020-03-14 10:41:52 +0500

    fixed dialog

--- qcsrc/menu/nexuiz/dialog_multiplayer_create.c
+++ qcsrc/menu/nexuiz/dialog_multiplayer_create.c
@@ -54,7 +54,7 @@ void fillNexuizServerCreateTab(entity me)
 		me.TD(me, 1, 3, e = makeNexuizTextLabel(0, "Map list:"));
 			makeCallback(e, me.mapListBox, me.mapListBox.refilterCallback);
 	me.TR(me);
-		me.TD(me, me.rows - 8, 3, me.mapListBox);
+		me.TD(me, me.rows - 9, 3, me.mapListBox);
 	me.gotoRC(me, me.rows - 3, 0);
 		me.TDempty(me, 0.5);
 		me.TD(me, 1, 1, e = makeNexuizButton("All", '0 0 0'));
