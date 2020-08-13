bcfd2d4966d6666456cdba575cbd3e37ca0a10e6
Grigorii Kasymov <kasymovga@gmail.com>
2020-08-13 11:18:09 +0500

    update network settings dialog

--- qcsrc/menu/nexuiz/dialog_settings_network.c
+++ qcsrc/menu/nexuiz/dialog_settings_network.c
@@ -21,11 +21,6 @@ entity makeNexuizNetworkSettingsTab()
 void fillNexuizNetworkSettingsTab(entity me)
 {
 	entity e;
-
-	me.TR(me);
-		me.TD(me, 1, 3, e = makeNexuizCheckBox(0, "cl_movement", "Client-side movement prediction"));
-	me.TR(me);
-		//me.TD(me, 1, 3, e = makeNexuizCheckBox(1, "cl_nolerp", "Network update smoothing"));
 	me.TR(me);
 		me.TD(me, 1, 3, e = makeNexuizCheckBox(0, "shownetgraph", "Show netgraph"));
 	me.TR(me);
@@ -37,10 +32,11 @@ void fillNexuizNetworkSettingsTab(entity me)
 			e.addValue(e, "Slow ADSL", "15000");
 			e.addValue(e, "Fast ADSL", "20000");
 			e.addValue(e, "Broadband", "25000");
+			e.addValue(e, "Unlimited", "128000");
 			e.configureNexuizTextSliderValues(e);
     me.TR(me);
 		me.TD(me, 1, 1, e = makeNexuizTextLabel(0, "Input packets/s:"));
-		me.TD(me, 1, 2, e = makeNexuizSlider(20, 100, 1, "cl_netfps"));
+		me.TD(me, 1, 2, e = makeNexuizSlider(20, 125, 1, "cl_netfps"));
 	me.TR(me);
 	me.TR(me);
 		me.TD(me, 1, 3, e = makeNexuizTextLabel(0, "HTTP downloads:"));
@@ -51,7 +47,7 @@ void fillNexuizNetworkSettingsTab(entity me)
 	me.TR(me);
 		me.TDempty(me, 0.2);
 		me.TD(me, 1, 0.8, e = makeNexuizTextLabel(0, "Speed (kB/s):"));
-		me.TD(me, 1, 2, e = makeNexuizSlider(10, 1500, 10, "cl_curl_maxspeed"));
+		me.TD(me, 1, 2, e = makeNexuizSlider(10, 5000, 10, "cl_curl_maxspeed"));
 	me.TR(me);
 	me.TR(me);
 		me.TD(me, 1, 1, e = makeNexuizTextLabel(0, "Client UDP port:"));
