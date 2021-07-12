acef64dc293e782bfce8d406862f815135e8b798
Grigorii Kasymov <kasymovga@gmail.com>
2021-07-12 20:01:03 +0500

    fixed radio button groups in input settings

--- qcsrc/menu/classes/nexuiz/dialog_settings_input.c
+++ qcsrc/menu/classes/nexuiz/dialog_settings_input.c
@@ -96,8 +96,8 @@ void fillNexuizInputSettingsTab(entity me)
 		me.TD(me, 1, 3, e = makeNexuizTextLabel(0, _("Show pressed keys:")));
 	me.TR(me);
 		me.TDempty(me, 0.1);
-		me.TD(me, 1, 1.05, e = makeNexuizRadioButton(1, "cl_showpressedkeys", "0", _("Never")));
-		me.TD(me, 1, 1.05, e = makeNexuizRadioButton(1, "cl_showpressedkeys", "1", _("In spectating")));
-		me.TD(me, 1, 1.05, e = makeNexuizRadioButton(1, "cl_showpressedkeys", "2", _("Always")));
+		me.TD(me, 1, 1.05, e = makeNexuizRadioButton(2, "cl_showpressedkeys", "0", _("Never")));
+		me.TD(me, 1, 1.05, e = makeNexuizRadioButton(2, "cl_showpressedkeys", "1", _("In spectating")));
+		me.TD(me, 1, 1.05, e = makeNexuizRadioButton(2, "cl_showpressedkeys", "2", _("Always")));
 }
 #endif
