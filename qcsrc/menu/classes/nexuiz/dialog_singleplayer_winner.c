009e0eb2774ae5e4260be9d26b4585ef2f638fa3
Grigorii Kasymov <kasymovga@gmail.com>
2021-08-31 20:26:20 +0500

    improved campaign win dialog

--- qcsrc/menu/classes/nexuiz/dialog_singleplayer_winner.c
+++ qcsrc/menu/classes/nexuiz/dialog_singleplayer_winner.c
@@ -1,25 +1,36 @@
 #ifdef INTERFACE
 CLASS(NexuizWinnerDialog) EXTENDS(NexuizDialog)
 	METHOD(NexuizWinnerDialog, fill, void(entity))
-	ATTRIB(NexuizWinnerDialog, title, string, _("Winner"))
+	ATTRIB(NexuizWinnerDialog, title, string, _("Congratulations!"))
 	ATTRIB(NexuizWinnerDialog, color, vector, SKINCOLOR_DIALOG_SINGLEPLAYER)
-	ATTRIB(NexuizWinnerDialog, intendedWidth, float, 0.32)
+	ATTRIB(NexuizWinnerDialog, intendedWidth, float, 0.4)
 	ATTRIB(NexuizWinnerDialog, rows, float, 12)
 	ATTRIB(NexuizWinnerDialog, columns, float, 3)
 ENDCLASS(NexuizWinnerDialog)
 #endif
 
 #ifdef IMPLEMENTATION
+
+void closeWinnderDialog(entity me, entity button)
+{
+	Dialog_Close(me, button);
+	menu_goto("credits");
+}
+
 void fillNexuizWinnerDialog(entity me)
 {
 	entity e;
 
 	me.TR(me);
-		me.TD(me, me.rows - 2, me.columns, e = makeNexuizImage("/gfx/winner", -1));
-
+		me.TDempty(me, 0.5);
+		me.TD(me, me.rows - 3, me.columns - 1, e = makeNexuizImage("/gfx/winner", -1));
+	me.gotoRC(me, me.rows - 3, 0);
+		me.TD(me, 1, me.columns, e = makeNexuizTextLabel(0, _("You have successfully completed the Single Player Campaign!")));
+		e.align = 0.5;
+		e.allowWrap = 1;
 	me.gotoRC(me, me.rows - 1, 0);
 		me.TD(me, 1, me.columns, e = makeNexuizButton(_("OK"), '0 0 0'));
-			e.onClick = Dialog_Close;
+			e.onClick = closeWinnderDialog;
 			e.onClickEntity = me;
 }
 #endif
