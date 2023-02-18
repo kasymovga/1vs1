2e7e91ed46b867377c601a861861323e26c06f8e
Grigorii Kasymov <kasymovga@gmail.com>
2023-02-18 21:42:19 +0500

    improved team count code

--- qcsrc/menu/classes/nexuiz/dialog_multiplayer_create_advanced.c
+++ qcsrc/menu/classes/nexuiz/dialog_multiplayer_create_advanced.c
@@ -50,13 +50,13 @@ void(entity me) fillNexuizAdvancedDialog {
 	me.TR(me);
 		me.TDempty(me, 0.2);
 		me.TD(me, 1, 1.2, e = makeNexuizTextLabel(0, _("Teams:")));
-		me.TD(me, 1, 1.7, e = makeNexuizTextSlider("g_tdm_teams_override"));
+		me.TD(me, 1, 1.7, e = makeNexuizTextSlider("g_tdm_teams"));
 			e.addValue(e, _("Default"), "0");
 			e.addValue(e, _("2 teams"), "2");
 			e.addValue(e, _("3 teams"), "3");
 			e.addValue(e, _("4 teams"), "4");
 			e.configureNexuizTextSliderValues(e);
-		gui_make_multi(e, "g_domination_teams_override g_keyhunt_teams_override");
+		gui_make_multi(e, "g_domination_teams g_kh_teams");
 	me.gotoRC(me, me.rows - 1, 0);
 		me.TD(me, 1, me.columns, e = makeNexuizButton(_("OK"), '0 0 0'));
 			e.onClick = Dialog_Close;
