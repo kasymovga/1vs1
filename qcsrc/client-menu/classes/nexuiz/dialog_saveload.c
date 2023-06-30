50fc6d74fac0810d30fb350bd9d4a935dad9f915
Grigorii Kasymov <kasymovga@gmail.com>
2023-06-30 21:01:52 +0500

    fallback for game loading

--- qcsrc/client-menu/classes/nexuiz/dialog_saveload.c
+++ qcsrc/client-menu/classes/nexuiz/dialog_saveload.c
@@ -68,6 +68,7 @@ void LoadGame(entity btn, entity me) {
 	float slot = GetSlotNumber(me);
 	if (SaveSlotNotEmpty(slot)) {
 		localcmd(strcat("\nset _restore_game 1; maxplayers 1; load slot", ftos(slot), "\n"));
+		localcmd(strcat("menu_cmd loadfallback slot", ftos(slot), ".sav\n"));
 		me.close(me);
 	}
 }
