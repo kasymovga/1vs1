8cd8a2490110b7ffd2f6e47ffb846b2d5ab96c60
Grigorii Kasymov <kasymovga@gmail.com>
2024-04-13 09:00:52 +0500

    translations

--- qcsrc/client-menu/classes/nexuiz/dialog_teamselect.c
+++ qcsrc/client-menu/classes/nexuiz/dialog_teamselect.c
@@ -2,10 +2,10 @@
 CLASS(NexuizTeamSelectDialog) EXTENDS(NexuizRootDialog)
 	METHOD(NexuizTeamSelectDialog, fill, void(entity)) // to be overridden by user to fill the dialog with controls
 	METHOD(NexuizTeamSelectDialog, showNotify, void(entity))
-	ATTRIB(NexuizTeamSelectDialog, title, string, "Team Selection") // ;)
+	ATTRIB(NexuizTeamSelectDialog, title, string, _("Team Selection")) // ;)
 	ATTRIB(NexuizTeamSelectDialog, color, vector, SKINCOLOR_DIALOG_TEAMSELECT)
 	ATTRIB(NexuizTeamSelectDialog, intendedWidth, float, 0.4)
-	ATTRIB(NexuizTeamSelectDialog, rows, float, 5)
+	ATTRIB(NexuizTeamSelectDialog, rows, float, 6)
 	ATTRIB(NexuizTeamSelectDialog, columns, float, 4)
 	ATTRIB(NexuizTeamSelectDialog, name, string, "TeamSelect")
 	ATTRIB(NexuizTeamSelectDialog, team1, entity, NULL)
@@ -38,17 +38,16 @@ void fillNexuizTeamSelectDialog(entity me)
 {
 	entity e;
 	me.TR(me);
-		me.TD(me, 2, 4, e = makeTeamButton("join 'best' team (auto-select)", '0 0 0', "cmd selectteam auto; cmd join"));
+		me.TD(me, 1.5, 4, e = makeTeamButton(_("join 'best' team (auto-select)"), '0 0 0', "cmd selectteam auto; cmd join"));
 			e.preferredFocusPriority = 1;
-	me.TR(me);
-	me.TR(me);
-		me.TD(me, 2, 1, me.team1 = makeTeamButton("red", '1 0.5 0.5', "cmd selectteam red; cmd join"));
-		me.TD(me, 2, 1, me.team2 = makeTeamButton("blue", '0.5 0.5 1', "cmd selectteam blue; cmd join"));
-		me.TD(me, 2, 1, me.team3 = makeTeamButton("yellow", '1 1 0.5', "cmd selectteam yellow; cmd join"));
-		me.TD(me, 2, 1, me.team4 = makeTeamButton("pink", '1 0.5 1', "cmd selectteam pink; cmd join"));
-	me.TR(me);
-	me.TR(me);
-		me.TD(me, 1, 4, makeNexuizCommandButton("spectate", '0 0 0', "cmd spectate", 1));
+	me.gotoRC(me, 1.5, 0);
+		me.TD(me, 1.5, 2, me.team1 = makeTeamButton(_("Red Team"), '1 0.5 0.5', "cmd selectteam red; cmd join"));
+		me.TD(me, 1.5, 2, me.team2 = makeTeamButton(_("Blue Team"), '0.5 0.5 1', "cmd selectteam blue; cmd join"));
+	me.gotoRC(me, 3, 0);
+		me.TD(me, 1.5, 2, me.team3 = makeTeamButton(_("Yellow Team"), '1 1 0.5', "cmd selectteam yellow; cmd join"));
+		me.TD(me, 1.5, 2, me.team4 = makeTeamButton(_("Pink Team"), '1 0.5 1', "cmd selectteam pink; cmd join"));
+	me.gotoRC(me, 4.5, 0);
+		me.TD(me, 1.5, 4, makeNexuizCommandButton(_("spectate"), '0 0 0', "cmd spectate", 1));
 }
 #endif
 
